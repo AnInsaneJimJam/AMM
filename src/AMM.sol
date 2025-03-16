3af97fa730792d42a68f4ba1828e4df6acc35584
12anand34 <anand47bansal#gmail.com>
2025-03-16 09:44:54 +0000

    added modifier and errors

--- src/AMM.sol
+++ src/AMM.sol
@@ -5,7 +5,10 @@ pragma solidity ^0.8.18;
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract AMM is ReentrancyGuard{
+contract AMM is ReentrancyGuard {
+    error AMM__ShouldBeMoreThanZero();
+    error AMM__InvalidToken(address token);
+
     IERC20 public tokenA;
     IERC20 public tokenB;
 
@@ -13,13 +16,25 @@ contract AMM is ReentrancyGuard{
     uint256 public balanceOfTokenB;
     uint256 public totalShares;
 
-    mapping (address user => uint256 shares) numberOfShares;
+    mapping(address user => uint256 shares) numberOfShares;
 
-    constructor(address _tokenA , address _tokenB){
+    modifier moreThanZero(uint256 amount) {
+        require(amount > 0, AMM__ShouldBeMoreThanZero());
+        _;
+    }
+
+    modifier validToken(address token) {
+        if (token == address(0) || (token != address(tokenA) && token != address(tokenB))) {
+            revert AMM__InvalidToken(token);
+        }
+        _;
+    }
+
+    constructor(address _tokenA, address _tokenB) {
         tokenA = IERC20(_tokenA);
         tokenB = IERC20(_tokenB);
     }
-    
+
     function _mintShares(address to, uint256 amount) internal {
         numberOfShares[to] += amount;
         totalShares += amount;
@@ -30,4 +45,5 @@ contract AMM is ReentrancyGuard{
         totalShares -= amount;
     }
 
+    function swap(address tokenIn, uint256 amountIn) public moreThanZero(amountIn) returns (uint256 amountOut) {}
 }
