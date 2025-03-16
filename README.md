799cf9747bebe90886e42b3bd1e21b4b220fa301
12anand34 <anand47bansal#gmail.com>
2025-03-16 18:36:59 +0000

    initial liquidity, remove liquidity, square root

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 2. 2 Tokens -  A and B
 3. Swap function
 4. Add liquidity->get shares
+    1.Handle initial liquidity
 5. Remove liquidity -> burn shares
--- src/AMM.sol
+++ src/AMM.sol
@@ -9,6 +9,9 @@ contract AMM is ReentrancyGuard {
     error AMM__ShouldBeMoreThanZero();
     error AMM__InvalidToken(address token);
     error AMM__InsufficientLiquidity();
+    error AMM__IncorrectRatioOfTokenProvidedForLiquidity();
+    error AMM__LiquidityAlreadySetUp();
+    error AMM__InsufficientSharesToBurn();
 
     IERC20 public immutable tokenA;
     IERC20 public immutable tokenB;
@@ -49,23 +52,6 @@ contract AMM is ReentrancyGuard {
         totalShares -= amount;
     }
 
-    function swap(address tokenIn, uint256 amountIn)
-        public
-        moreThanZero(amountIn)
-        validToken(tokenIn)
-        returns (uint256 amountOut)
-    {
-        (address tokenOut, uint256 reserveIn, uint256 reserveOut) = _selectSides(tokenIn);
-        amountOut = (reserveOut * amountIn) / (reserveIn + amountIn);
-        if (amountOut >= reserveOut) {
-            revert AMM__InsufficientLiquidity();
-        }
-        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
-        IERC20(tokenOut).transferFrom(address(this), msg.sender, amountOut);
-
-        _updateReserve(tokenA.balanceOf(address(this)), tokenB.balanceOf(address(this)));
-    }
-
     function _selectSides(address tokenIn)
         internal
         view
@@ -86,4 +72,60 @@ contract AMM is ReentrancyGuard {
         reserveOfTokenA = _reserveA;
         reserveOfTokenB = _reserveB;
     }
+
+    function initialLiquidity(uint256 amountA, uint256 amountB) public moreThanZero(amountA) moreThanZero(amountB) returns (uint256 initialShares){
+        require(reserveOfTokenA == 0 && reserveOfTokenB == 0, AMM__LiquidityAlreadySetUp());
+        initialShares = sqrt(amountA*amountB);
+        _mintShares(msg.sender,initialShares);
+        _updateReserve(amountA, amountB);
+
+    }
+
+    function swap(address tokenIn, uint256 amountIn)
+        public
+        moreThanZero(amountIn)
+        validToken(tokenIn)
+        returns (uint256 amountOut)
+    {
+        (address tokenOut, uint256 reserveIn, uint256 reserveOut) = _selectSides(tokenIn);
+        amountOut = (reserveOut * amountIn) / (reserveIn + amountIn);
+        if (amountOut >= reserveOut) {
+            revert AMM__InsufficientLiquidity();
+        }
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenOut).transferFrom(address(this), msg.sender, amountOut);
+
+        _updateReserve(tokenA.balanceOf(address(this)), tokenB.balanceOf(address(this)));
+    }
+
+    function addLiquidity(uint256 amountA, uint256 amountB) public moreThanZero(amountA) moreThanZero(amountB) returns (uint256 shares){
+        require(amountA/amountB == reserveOfTokenA/reserveOfTokenB, AMM__IncorrectRatioOfTokenProvidedForLiquidity());
+        shares = (amountA/reserveOfTokenA)*totalShares;
+        _updateReserve(amountA+reserveOfTokenA, amountB+reserveOfTokenB);
+        _mintShares(msg.sender, shares);    
+    }
+
+    function removeLiquidity(address user, uint256 sharesToBurn) public moreThanZero(shares) {
+        require(numberOfShares[user] >= sharesToBurn, AMM__InsufficientSharesToBurn());
+        uint256 tokenAout = (reserveOfTokenA*sharesToBurn)/totalShares;
+        uint256 tokenBout = (reserveOfTokenA*sharesToBurn)/totalShares;
+        tokenA.transfer(user,tokenAOut);
+        tokenB.transfer(user,tokenBOut);
+        _burnShares(user, sharesToBurn);
+        _updateReserve(reserveOfTokenA-tokenAout, reserveOfTokenB-tokenBout);
+    }
+
+    // Taken from uinswap V2 diocs
+    function sqrt(uint y) internal pure returns (uint z) {
+    if (y > 3) {
+        z = y;
+        uint x = y / 2 + 1;
+        while (x < z) {
+            z = x;
+            x = (y / x + x) / 2;
+        }
+    } else if (y != 0) {
+        z = 1;
+    }
+}
 }
