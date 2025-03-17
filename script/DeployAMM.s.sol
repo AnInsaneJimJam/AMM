0ef209991ac3939c8a5d4b955ccd0dfbe23763bc
12anand34 <anand47bansal#gmail.com>
2025-03-17 05:16:58 +0000

    return change in deploy script

--- script/DeployAMM.s.sol
+++ script/DeployAMM.s.sol
@@ -7,13 +7,12 @@ import {AMM} from "../src/AMM.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DeployAMM is Script {
-    function run() external returns (AMM) {
+    function run() external returns (AMM amm, ERC20Mock tokenA, ERC20Mock tokenB) {
         vm.startBroadcast();
-        ERC20Mock tokenA = new ERC20Mock();
-        ERC20Mock tokenB = new ERC20Mock();
+        tokenA = new ERC20Mock();
+        tokenB = new ERC20Mock();
 
-        AMM amm = new AMM(address(tokenA), address(tokenB));
+        amm = new AMM(address(tokenA), address(tokenB));
         vm.stopBroadcast();
-        return amm;
     }
 }
